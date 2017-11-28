130f6f88a41fcd7d2d681e8372cbb6a5404ceb26
Kotaro Mochizuki <mochizuki@basicinc.jp>
2017-11-28 15:06:26 +0900

    Add Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: rollup -c rollup.config.production.js && npm start
---
+++ rollup.config.production.js
@@ -0,0 +1,25 @@
+import resolve  from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel  from 'rollup-plugin-babel'
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+    moduleName: 'foo'
+  },
+  watch: {
+    include: 'src/**'
+  },
+  plugins: [
+    resolve({
+      jsnext: true,
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
+    commonjs(), // CommonJSモジュールをES6に変換
+    babel(), // ES5に変換
+  ]
+}
